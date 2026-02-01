970808a1c29f4bb3b421b4316abbed8fc4630e99
Vladislav <vladislav.mikkoev@yandex.ru>
2026-02-01 17:54:23 +0300

    Убрать часть кода

--- src/Algorithms.BinarySearch/Program.cs
+++ src/Algorithms.BinarySearch/Program.cs
@@ -4,16 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        Console.WriteLine(test.CustomBinarySearch(1));
-        Console.WriteLine(test.CustomBinarySearch(2));
-        Console.WriteLine(test.CustomBinarySearch(3));
-        Console.WriteLine(test.CustomBinarySearch(4));
-        Console.WriteLine(test.CustomBinarySearch(5));
-        Console.WriteLine(test.CustomBinarySearch(6));
-        Console.WriteLine(test.CustomBinarySearch(7));
-        Console.WriteLine(test.CustomBinarySearch(8));
-        Console.WriteLine(test.CustomBinarySearch(9));
-        Console.WriteLine(test.CustomBinarySearch(10));
+        Console.WriteLine("Hello, World!");
     }
 }
